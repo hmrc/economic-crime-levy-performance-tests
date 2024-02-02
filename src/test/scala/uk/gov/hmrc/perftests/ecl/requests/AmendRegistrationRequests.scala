4470ad705acb1c7d0c069c3ab55766bd96351832
jamesvarghese701 <113344779+jamesvarghese701@users.noreply.github.com>
2024-02-02 15:37:54 +0000

    ECL-667 amend registration and returns

--- src/test/scala/uk/gov/hmrc/perftests/ecl/requests/AmendRegistrationRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/ecl/requests/AmendRegistrationRequests.scala
@@ -27,25 +27,24 @@ object AmendRegistrationRequests extends Configuration {
   def redirectLocation(relativeLocation: String): String =
     s"$registrationUrl/$relativeLocation"
 
-  val eclReferenceNumber                         = AuthRequests.identifierValue
-  val amendEclRegistrationUrl: String            = s"$registerAuthWizardUrl/amend-economic-crime-levy-registration/$identifierValue"
+  val amendEclRegistrationUrl: String            = s"$registerAuthWizardUrl/amend-economic-crime-levy-registration/XMECL0000000002"
   val whoIsYourAmendAmlSupervisorUrl: String     = s"$registerAuthWizardUrl/your-aml-supervisor/Amendment?fromLiableBeforeCurrentYearPage=false"
   val contactUkAddressUrl: String                = s"$registerAuthWizardUrl/uk-address"
   val stubAlfJourneyDataUrl: String =
     s"$registerAuthWizardUrl/test-only/stub-alf-journey-data?continueUrl=normalmode"
   val amendRegistrationSubmittedUrl: String      = s"$registerAuthWizardUrl/amend-economic-crime-levy-registration/success/confirmation"
-  val ReasonForAmendRegistrationUrl: String      = s"$registerAuthWizardUrl/why-are-you-amending-your-registration"
+  val ReasonForAmendRegistrationUrl: String      = s"$registerAuthWizardUrl/change-answer/why-are-you-amending-your-registration"
 
 
   val navigateToEclAccount: HttpRequestBuilder =
-    http(s"Navigate to /amend-economic-crime-levy-registration/$identifierValue")
+    http(s"Navigate to /amend-economic-crime-levy-registration/XMECL0000000002")
       .get(accountAuthWizardUrl)
       .check(status.is(200))
 
   val navigateToSubmitAmendRegistration: HttpRequestBuilder =
-    http(s"Navigate to /amend-economic-crime-levy-registration/$identifierValue")
+    http(s"Navigate to /amend-economic-crime-levy-registration/XMECL0000000002")
       .get(amendEclRegistrationUrl)
-      .check(status.is(200))
+      .check(status.is(303))
 
   val navigateToReasonForAmendRegistration: HttpRequestBuilder =
     http("Navigate to Reason for Amend return start page")
@@ -58,193 +57,6 @@ object AmendRegistrationRequests extends Configuration {
       .post(ReasonForAmendRegistrationUrl)
       .formParam("csrfToken", "${csrfToken}")
       .formParam("value", reason)
-      .check(status.is(303))
-
-  val navigateToAmendAmlSupervisor: HttpRequestBuilder =
-    http("Navigate to /who-is-your-aml-supervisor/Amendment")
-      .get(whoIsYourAmendAmlSupervisorUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendAmlSupervisor(amlSupervisor: String): HttpRequestBuilder =
-    http("Submit amend AML Supervisor: " + amlSupervisor)
-      .post(whoIsYourAmendAmlSupervisorUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", amlSupervisor)
-      .formParam("otherProfessionalBody", "")
-      .check(status.is(303))
-
-  val navigateToYourAmendBusinessSector: HttpRequestBuilder =
-    http("Navigate to amend /what-is-your-business-sector")
-      .get(RegistrationRequests.whatIsYourBusinessSectorUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitYourAmendBusinessSector(businessSector: String): HttpRequestBuilder =
-    http("Submit amend Business sector: " + businessSector)
-      .post(RegistrationRequests.whatIsYourBusinessSectorUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", businessSector)
-      .check(status.is(303))
-
-  val navigateToAmendFirstContactPersonName: HttpRequestBuilder =
-    http("Navigate to amend /contact-name")
-      .get(RegistrationRequests.firstContactPersonNameUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendFirstContactPersonName(contactName: String): HttpRequestBuilder =
-    http("Submit amend contact person's Name: " + contactName)
-      .post(RegistrationRequests.firstContactPersonNameUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", contactName)
-      .check(status.is(303))
-
-  val navigateToAmendFirstContactPersonRole: HttpRequestBuilder =
-    http("Navigate to amend /contact-role")
-      .get(RegistrationRequests.firstContactPersonRoleUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendFirstContactPersonRole(contactRole: String): HttpRequestBuilder =
-    http("Submit amend contact person's Role: " + contactRole)
-      .post(RegistrationRequests.firstContactPersonRoleUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", contactRole)
-      .check(status.is(303))
-
-  val navigateToAmendFirstContactPersonEmail: HttpRequestBuilder =
-    http("Navigate to amend /contact-email-address")
-      .get(RegistrationRequests.firstContactPersonEmailUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendFirstContactPersonEmail(emailAddress: String): HttpRequestBuilder =
-    http("Submit amend contact person's Email address: " + emailAddress)
-      .post(RegistrationRequests.firstContactPersonEmailUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", emailAddress)
-      .check(status.is(303))
-
-  val navigateToAmendFirstContactPersonTelephone: HttpRequestBuilder =
-    http("Navigate to amend /contact-Telephone")
-      .get(RegistrationRequests.firstContactPersonTelephoneUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendFirstContactPersonTelephone(contactNumber: String): HttpRequestBuilder =
-    http("Submit amend contact person's Telephone: " + contactNumber)
-      .post(RegistrationRequests.firstContactPersonTelephoneUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", contactNumber)
-      .check(status.is(303))
-
-  val navigateToAmendWouldYouLikeToAddAnotherContact: HttpRequestBuilder             =
-    http("Navigate to amend /second-contact")
-      .get(RegistrationRequests.wouldYouLikeToAddAnotherContactUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendWouldYouLikeToAddAnotherContact(answer: String): HttpRequestBuilder =
-    http("Submit add another contact: " + answer)
-      .post(RegistrationRequests.wouldYouLikeToAddAnotherContactUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", answer)
-      .check(status.is(303))
-
-  val navigateToAmendSecondContactPersonName: HttpRequestBuilder =
-    http("Navigate to amend /second-contact-name")
-      .get(RegistrationRequests.secondContactPersonNameUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendSecondContactPersonName(contactName: String): HttpRequestBuilder =
-    http("Submit amend second contact person's Name: " + contactName)
-      .post(RegistrationRequests.secondContactPersonNameUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", contactName)
-      .check(status.is(303))
-
-  val navigateToAmendSecondContactPersonRole: HttpRequestBuilder =
-    http("Navigate to amend /second-contact-role")
-      .get(RegistrationRequests.secondContactPersonRoleUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendSecondContactPersonRole(contactRole: String): HttpRequestBuilder =
-    http("Submit amend second contact person's Role: " + contactRole)
-      .post(RegistrationRequests.secondContactPersonRoleUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", contactRole)
-      .check(status.is(303))
-
-  val navigateToAmendSecondContactPersonEmail: HttpRequestBuilder =
-    http("Navigate to amend /second-contact-email-address")
-      .get(RegistrationRequests.secondContactPersonEmailUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendSecondContactPersonEmail(emailAddress: String): HttpRequestBuilder =
-    http("Submit amend second contact person's Email address: " + emailAddress)
-      .post(RegistrationRequests.secondContactPersonEmailUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", emailAddress)
-      .check(status.is(303))
-
-  val navigateToAmendSecondContactPersonTelephone: HttpRequestBuilder =
-    http("Navigate to amend /second-contact-Telephone")
-      .get(RegistrationRequests.secondContactPersonTelephoneUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendSecondContactPersonTelephone(contactNumber: String): HttpRequestBuilder =
-    http("Submit amend second contact person's Telephone: " + contactNumber)
-      .post(RegistrationRequests.secondContactPersonTelephoneUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", contactNumber)
-      .check(status.is(303))
-
-  val navigateToWhetherOrNotContactAddressInUk: HttpRequestBuilder =
-    http("Navigate to amend /contact-address")
-      .get(contactUkAddressUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitWhetherOrNotContactAddressInUk(answer: String): HttpRequestBuilder =
-    http("Submit amend registered contact address: " + answer)
-      .post(contactUkAddressUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", answer)
-      .check(status.is(303))
-
-  val navigateToStubAlfJourneyData: HttpRequestBuilder =
-    http("Navigate to /test-only/stub-alf-journey-data")
-      .get(stubAlfJourneyDataUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitStubAlfJourneyData(): HttpRequestBuilder =
-    http("Stub ALF Journey Data")
-      .post(stubAlfJourneyDataUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("addressJson",
-        """{
-           "address" : {
-           "lines" : [ "Address Line 1", "Address Line 2", "Address Line 3", "Test Town" ],
-           "postcode" : "AB12 3CD",
-           "country" : {
-           "code" : "GB",
-           "name" : "United Kingdom"
-        }
-      }
-    }"""
-      )
-      .check(status.is(303))
-      .check(header("Location").saveAs("alfContinueLocation"))
-
-  val navigateToAlfContinue: HttpRequestBuilder =
-    http("Navigate to /alf-continue/normalmode")
-      .get(redirectLocation("${alfContinueLocation}"))
       .check(status.is(303))
 
     val navigateToAmendSubmitCheckYourAnswers: HttpRequestBuilder =
--- src/test/scala/uk/gov/hmrc/perftests/ecl/requests/AmendReturnsRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/ecl/requests/AmendReturnsRequests.scala
@@ -26,12 +26,12 @@ object AmendReturnsRequests extends Configuration {
 
   val startAmendReturnUrl: String          = s"$returnAuthWizardUrl/amend/22XY/XMECL0000000006"
   val amendReturnSubmittedUrl: String      = s"$returnAuthWizardUrl/amend/confirmation"
-  val ReasonForAmendReturnUrl: String      = s"$returnAuthWizardUrl/can-you-provide-more-detail"
+  val ReasonForAmendReturnUrl: String      = s"$returnAuthWizardUrl/change-answer/can-you-provide-more-detail"
 
   val navigateToAmendStartReturn: HttpRequestBuilder =
     http("Navigate to Amend return start page")
       .get(startAmendReturnUrl)
-      .check(status.is(200))
+      .check(status.is(303))
 
   val navigateToReasonForAmendReturn: HttpRequestBuilder =
     http("Navigate to Reason for Amend return start page")
@@ -46,107 +46,6 @@ object AmendReturnsRequests extends Configuration {
       .formParam("value", reason)
       .check(status.is(303))
 
-  val navigateToAmendIsRelevantAccountingPeriod12MonthsUrl: HttpRequestBuilder =
-    http("Navigate to amend /is-relevant-accounting-period-12-months")
-      .get(ReturnsRequests.isRelevantAccountingPeriod12MonthsUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendIsYourRelevantAccountingPeriod12Months(answer: String): HttpRequestBuilder =
-    http("Amend is UK Revenue for the relevant accounting period 12 months: " + answer)
-      .post(ReturnsRequests.isRelevantAccountingPeriod12MonthsUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", answer)
-      .check(status.is(303))
-
-  val navigateToAmendUkRevenueAccountingPeriod: HttpRequestBuilder =
-    http("Navigate to amend /uk-revenue-in-accounting-period")
-      .get(ReturnsRequests.ukRevenueInAccountingPeriodUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendUkRevenueForTheRelevantAccountingPeriod(ukRevenue: String): HttpRequestBuilder =
-    http("Amend Is UK Revenue for the relevant accounting period: " + ukRevenue)
-      .post(ReturnsRequests.ukRevenueInAccountingPeriodUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", ukRevenue)
-      .check(status.is(303))
-
-  val navigateToAmendAmlRegulatedActivity: HttpRequestBuilder             =
-    http("Navigate to amend /aml-regulated-activity")
-      .get(ReturnsRequests.amlRegulatedActivityUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-  def submitAmendAmlRegulatedActivity(answer: String): HttpRequestBuilder =
-    http("Amend Did you carry out AML-regulated activity for the full financial year?: " + answer)
-      .post(ReturnsRequests.amlRegulatedActivityUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", answer)
-      .check(status.is(303))
-
-  val navigateToAmendAmountDue: HttpRequestBuilder =
-    http("Navigate to amend /amount-due")
-      .get(ReturnsRequests.amountDueUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendAmountDue(): HttpRequestBuilder =
-    http("Amend Amount Due ")
-      .post(ReturnsRequests.amountDueUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .check(status.is(303))
-
-  val navigateToAmendContactName: HttpRequestBuilder =
-    http("Navigate to amend /contact-name")
-      .get(ReturnsRequests.contactNameUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendContactName(contactName: String): HttpRequestBuilder =
-    http("Contact Name: " + contactName)
-      .post(ReturnsRequests.contactNameUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", contactName)
-      .check(status.is(303))
-
-  val navigateToAmendContactRole: HttpRequestBuilder =
-    http("Navigate to amend /contact-role")
-      .get(ReturnsRequests.contactRoleUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendContactRole(contactRole: String): HttpRequestBuilder =
-    http("Contact Role: " + contactRole)
-      .post(ReturnsRequests.contactRoleUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", contactRole)
-      .check(status.is(303))
-
-  val navigateToAmendContactEmailAddress: HttpRequestBuilder =
-    http("Navigate to amend /contact-email-address")
-      .get(ReturnsRequests.contactEmailUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendContactEmailAddress(contactEmail: String): HttpRequestBuilder =
-    http("Contact Email: " + contactEmail)
-      .post(ReturnsRequests.contactEmailUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", contactEmail)
-      .check(status.is(303))
-
-  val navigateToAmendContactTelephone: HttpRequestBuilder =
-    http("Navigate to amend /contact-telephone")
-      .get(ReturnsRequests.contactTelephoneUrl)
-      .check(status.is(200))
-      .check(saveCsrfToken)
-
-  def submitAmendContactTelephone(contactTelephone: String): HttpRequestBuilder =
-    http("Contact Number: " + contactTelephone)
-      .post(ReturnsRequests.contactTelephoneUrl)
-      .formParam("csrfToken", "${csrfToken}")
-      .formParam("value", contactTelephone)
-      .check(status.is(303))
 
   val navigateToAmendCheckYourAnswers: HttpRequestBuilder =
     http("Navigate to amend /check-your-answers")
