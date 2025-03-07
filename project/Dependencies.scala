12b423daa2c9d64dbb83b5baebb82148774eb521
lewis-nicholls <173899555+lewis-nicholls@users.noreply.github.com>
2025-03-07 15:54:37 +0000

    tidying up of dependencies.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  private val gatlingVersion = "3.6.1"
-
-  val test: Seq[ModuleID] = Seq(
-    "com.typesafe"          % "config"                    % "1.4.3"        % Test,
+  val test = Seq(
     "uk.gov.hmrc"          %% "performance-test-runner"   % "6.1.0"        % Test
   )
 }
